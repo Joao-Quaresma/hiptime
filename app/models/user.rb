f8723d053725a8803418341213ac06c7c27f98c3
Joao Quaresma <joaoquaresma@Joaos-MacBook-Pro-3.local>
2018-07-30 16:46:16 +0100

    add devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# user class
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
